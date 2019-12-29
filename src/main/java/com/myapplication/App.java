31cc5048733001385dd49dc84d20eef640b479ce
venkataramana <venkataramanasandhireddy@gmail.com>
2019-12-29 17:17:31 +0530

    my first commit

---
+++ src/main/java/com/myapplication/App.java
@@ -0,0 +1,13 @@
+package com.myapplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
